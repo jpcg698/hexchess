73ad476f09301d698dba48ee2b6c5bb2c169b04f
Juan Pablo <juanpcortesg@gmail.com>
2023-07-30 15:35:34 -0600

    has a rook in the middle, better colors

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,44 +1,49 @@
 'use client'
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Hex from "../../components/hex"
 var a = "hello"
 interface cell {
     row:number;
     col:number;
-    active:boolean
+    active:boolean;
+    piece:string;
 }
 var funnelHeight = 6
 var midLength = 6
 var heightActual = 2*midLength + 2*funnelHeight - 3 ///always odd
 var midLengthActual = 2*midLength -1 // always odd
 var board:Array<Array<cell>> = []
-// for(let i = 0;i<funnelHeight;i++){
-//     let currRow = []
-//     let prevActive = false
-//     for(let j = 0; j < (2*i + 1) ;j++){
-//         prevActive = !prevActive
-//         let currCell = {row:i+1, col:midLength-i+j, active:prevActive}
-//         currRow.push(currCell)
-//     }
-//     board.push(currRow)
-// }
+
 
 const checkIfActive = (row:number,col:number) => {
     return ( ((row + col)%2 > 0) && (row + col > midLength) && (row + col < (heightActual + midLength + 1)) && (col - row < midLength) && (col - row > (-1 * (heightActual - midLength + 1))) )
 }
 
-for(let i = 0; i < heightActual;i++){
-    let currRow = []
-    for(let j = 0; j< midLengthActual;j++){
-        let currCell = {row:i+1, col:j+1, active:checkIfActive(i+1,j+1), colmrow:j+1-(i+1)}
-        currRow.push(currCell)
-    }
-    board.push(currRow)
-}
+
 export default function Play(){
+    const [board,setBoard] = useState<Array<Array<cell>>>([[]])
+
+    function initializeBoard(){
+        let initBoard = []
+        for(let i = 0; i < heightActual;i++){
+            let currRow = []
+            for(let j = 0; j< midLengthActual;j++){
+                let currCell = {row:i+1, col:j+1, active:checkIfActive(i+1,j+1), piece:""}
+                currRow.push(currCell)
+            }
+            initBoard.push(currRow)
+        }
+        initBoard[6][5].piece = "rook"
+        console.log(initBoard)
+        setBoard(initBoard)
+    }
+
     useEffect(()=>{
-        console.log(board)
+        initializeBoard()
     },[])
+
+
+
     return(
         <main className="">
             <div className="self-center grid">
--- src/components/hex.tsx
+++ src/components/hex.tsx
@@ -1,14 +1,37 @@
 import React from "react";
+import Image from "next/image";
+export default function Tile (prop: { info: { active: any; row:number; piece:string}; }){
 
-export default function Tile (prop: { info: { active: any; }; }){
+    let color
+    let rowMod = prop.info.row % 3
+    let bgrook = "./../"
+    let pieceImage : any = {
+        rook:"./Chess_rdt45.svg"
+    }
+
+    switch(rowMod){
+        case 0:
+            color = "#FFCF9E"
+            break
+        case 1:
+            color = "#E8AC6F"
+            break
+        case 2:
+            color = "#D18C47"
+            break
+    }
     if(prop.info.active){
         return (
-            <div className="inline-block -mt-10 ml-10">
-                <svg width={"5rem"} height={"5rem"} viewBox="0 0 100 100">
-                <polygon fill="#F7941E" stroke="#231F20" stroke-width="1" 
-                stroke-miterlimit="10" points="25 0, 75 0, 100 50, 75 100,25 100, 0 50" opacity="0.6" />
-                </svg>
-            </div>
+                <div className="inline-block -mt-10 ml-10 w-20 h-20 relative">
+                    <svg className="absolute top-0 left-0" width="100%" height="100%" viewBox="0 0 100 100">
+                    <polygon fillOpacity={1} fill={color} stroke="#231F20" strokeWidth="0.1" 
+                    strokeMiterlimit="10" points="25 0, 75 0, 100 50, 75 100,25 100, 0 50" opacity="0.6" />
+                    </svg>
+                    { prop.info.piece.length > 0 &&
+                            <Image className="absolute block w-3/4 max-w-full h-3/4 z-10 top-2 left-2.5" width={0} height={0} src={pieceImage[prop.info.piece]} alt=""/>
+                            /* <Image className="block w-full max-w-full h-auto z-10" width={0} height={0} src="/./Chess_rdt60.png" alt=""/> */
+                    }
+                </div>
             )
     }else{
         return(
